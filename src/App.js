ea539cab4eec3961396839ae0c2fd1acc523703d
Tyler Collier <tyler.collier+github@devmounta.in>
2018-04-09 11:04:20 -0700

    Initial commit: responsive header

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="main-nav">
+          <div className="logo">START BOOTSTRAP</div>
+          <div className="links">MENU</div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
