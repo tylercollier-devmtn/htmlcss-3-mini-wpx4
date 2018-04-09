4dcc3a1568e9a0a3e5820e4515e78f806286cdcf
Tyler Collier <tyler.collier+github@devmounta.in>
2018-04-09 12:36:41 -0700

    Animate drawer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import classnames from 'classnames';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAnimating: false,
+      showDrawer: false,
+    };
+  }
+
   render() {
     return (
       <div>
         <nav className="main-nav">
           <div className="logo">START BOOTSTRAP</div>
           <div className="links">
-            <div className="phone">MENU</div>
+            <div className="phone">
+              <button
+                onClick={() => this.setState({ showDrawer: !this.state.showDrawer})}
+              >Menu</button>
+            </div>
             <div className="desktop">ABOUT DOWNLOAD CONTACT</div>
           </div>
         </nav>
         <main>
-          <div className="square animation2 animation1">I am an L7</div>
+          <button onClick={() => this.setState({ isAnimating: !this.state.isAnimating })}>Click to animate!</button>
+          <div className={this.state.isAnimating ? 'square animation1' : 'square'}>I am an L7</div>
         </main>
+        <div className={classnames('drawer', { open: this.state.showDrawer })}>
+          <button
+            onClick={() => this.setState({ showDrawer: !this.state.showDrawer})}
+          >Close</button><br />
+          ABOUT<br />
+          DOWNLOAD<br />
+          CONTACT<br />
+        </div>
       </div>
     );
   }
